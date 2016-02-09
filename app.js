013724a7305aaf353c67629e19a5c09fdf04a113
Xinyuan Zhang <xiz016@ucsd.edu>
2016-02-09 01:15:52 +0000

    ajax lab

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var project = require('./routes/project');
+var palette = require('./routes/palette');
 // Example route
 // var user = require('./routes/user');
 
@@ -40,6 +41,7 @@ app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
 // Example route
 // app.get('/users', user.list);
+app.get('/palette', palette.randomPalette);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,6 +14,8 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+
+
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
@@ -27,7 +29,28 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/"+idNumber, addProject);
+	console.log("/project/" + idNumber);
+
 }
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML = 
+						'<img src=' + result['image'] + ' class="detailsImage">' +
+						'<p>' + result['title'] + '</p>' +
+						'<p><small>' + result['date'] + '</small></p></a>' +
+						'<p>' + result['summary'] + '</p>';
+						
+	var project = $("#project"+result["id"]);
+	var detail = $(project).find(".details");
+	$(detail).html(projectHTML);
+	//$("#project-descriptino").html(result['summary']);
+}
+
+
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +58,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", addColor);
+
+}
+
+function addColor(result) {
+	console.log(result);
+	var colors = result['colors']['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
 }
